b0f877927789ddc656f7a782fb77fad37a14ed53
Hikaru Yoshino <osushicrusher@gmail.com>
2023-02-26 01:06:09 +0900

    feat(service worker): update

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,19 +1,14 @@
-import browser from 'webextension-polyfill';
-
-import store from '../app/store';
-import { isDev } from '../shared/utils';
-
-store.subscribe(() => {
-  console.log('state', store.getState());
-});
-
-// show welcome page on new install
-browser.runtime.onInstalled.addListener(async (details) => {
-  if (details.reason === 'install') {
-    //show the welcome page
-    const url = browser.runtime.getURL(isDev ? 'src/welcome/welcome.html' : 'welcome.html'); // TODO: better approach
-    await browser.tabs.create({ url });
-  }
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'A',
+    title: '(A)常に表示',
+    contexts: ['all'],
+  });
+  chrome.contextMenus.create({
+    id: 'B',
+    title: '(B)選択時のみ表示',
+    contexts: ['selection'],
+  });
 });
 
 export {};
