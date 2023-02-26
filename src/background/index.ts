bb27f16da7af0710b41ba04d006ba2ba78f9f735
Hikaru Yoshino <osushicrusher@gmail.com>
2023-02-26 11:25:54 +0900

    feat: サービスワーカーの雛形作成

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,14 +1,24 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
-    id: 'A',
-    title: '(A)常に表示',
-    contexts: ['all'],
-  });
-  chrome.contextMenus.create({
-    id: 'B',
-    title: '(B)選択時のみ表示',
+    id: 'translation',
+    title: '選択したテキストを翻訳',
     contexts: ['selection'],
   });
+});
+
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+  if (tab !== undefined) {
+    switch (info.menuItemId) {
+      case 'translation':
+        // TODO ここのselectionTextでGPT→Notionを行う
+        // https://zenn.dev/alvinvin/books/chrome_extension/viewer/chapter09
+        // Notion APIのPOSTが完了したら成功モーダルみたいの出す
+        // その際に、サービスワーカーからコンテンツスクリプトに成功結果をメッセージ送信して結果を表示する
+        // https://zenn.dev/alvinvin/books/chrome_extension/viewer/chapter11
+        console.log(info.selectionText);
+        break;
+    }
+  }
 });
 
 export {};
