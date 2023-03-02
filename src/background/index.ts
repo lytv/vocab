2fa38f3932a7355fa53045ed0ea366777611ac7d
Hikaru Yoshino <osushicrusher@gmail.com>
2023-03-03 00:41:19 +0900

    feat(background): update background

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,5 @@
+import { getCompletion } from '../app/completion';
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: 'translation',
@@ -6,17 +8,37 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
+// chrome.runtime.onMessage.addListener(async function (message, sender, sendResponse) {});
+
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-  if (tab !== undefined) {
-    switch (info.menuItemId) {
-      case 'translation':
-        // TODO ここのselectionTextでGPT→Notionを行う
-        // https://zenn.dev/alvinvin/books/chrome_extension/viewer/chapter09
-        // Notion APIのPOSTが完了したら成功モーダルみたいの出す
-        // その際に、サービスワーカーからコンテンツスクリプトに成功結果をメッセージ送信して結果を表示する
-        // https://zenn.dev/alvinvin/books/chrome_extension/viewer/chapter11
-        console.log(info.selectionText);
-        break;
+  if (tab === undefined || info.selectionText === undefined) return;
+  const { menuItemId, selectionText } = info;
+  const lowerCasedText = selectionText.toLocaleLowerCase('en-US');
+  switch (menuItemId) {
+    case 'translation': {
+      const data = await getCompletion(lowerCasedText);
+      console.log(data);
+
+      // const data = {
+      //   completion: {
+      //     example_1: 'The leopard was spotted with black spots.',
+      //     example_2: 'The tablecloth was spotted with red wine.',
+      //     meaning: 'covered with spots',
+      //     parts_of_speech: 'adjective',
+      //     synonym: 'test',
+      //     word: 'spotted',
+      //   },
+      //   finish_reason: 'stop',
+      // };
+      const { completion, finish_reason } = data;
+      chrome.tabs.sendMessage(tab.id as number, {
+        type: 'SHOW',
+        data: {
+          completion,
+          finish_reason,
+        },
+      });
+      break;
     }
   }
 });
