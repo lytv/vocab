71158b7fa87ebb7cdc2c973230f3bcb35bfbbeda
Hikaru Yoshino <osushicrusher@gmail.com>
2023-03-03 00:39:33 +0900

    feat(page): update content

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,20 +1,18 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { Store } from '@eduardoac-skimlinks/webext-redux';
 
-import { proxyStore as store } from '../app/proxyStore';
+import { Main } from './Main';
 
-import Content from './Content';
-
-withProxyStore(<Content />, store).then((component) => {
-  const container = document.createElement('my-extension-root');
-  document.body.append(container);
-  createRoot(container).render(component);
+chrome.runtime.onMessage.addListener(async function (message, sender, sendResponse) {
+  if (message.type === 'SHOW') {
+    if (document.getElementsByTagName('my-extension-root').length > 0) {
+      document.getElementsByTagName('my-extension-root')[0].remove();
+    }
+    const { completion, finish_reason } = message.data;
+    const container = document.createElement('my-extension-root');
+    container.classList.add('parentclass');
+    document.body.after(container);
+    document.body.style.overflow = 'hidden';
+    createRoot(container).render(<Main completion={completion} finish_reason={finish_reason} />);
+  }
 });
-
-async function withProxyStore(children: ReactElement, proxyStore: Store): Promise<ReactElement> {
-  return proxyStore.ready().then(() => {
-    return <Provider store={proxyStore}>{children}</Provider>;
-  });
-}
