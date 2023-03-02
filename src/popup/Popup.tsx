101b6f61609bdcd45f7326a947eb15f3f2dcdb3a
Hikaru Yoshino <osushicrusher@gmail.com>
2023-03-03 00:38:26 +0900

    feat(Page): update Popup

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -8,7 +8,7 @@ const Popup = (): ReactElement => {
       <header className="bg-red-300">
         <nav className="mx-auto flex max-w-7xl items-center justify-between" aria-label="Global">
           <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
+            <a href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img
                 className="h-8 w-auto"
